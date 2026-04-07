1287be14a48e7135d386632bbd8b2b53096dc4f9
Andrei Cartera <andrei_cartera@outlook.com>
2026-04-07 17:09:51 +0300

    removed CSharpCourse, started GameLeaderboard

--- GameLeaderboard/GameLeaderboard/Properties/AssemblyInfo.cs
+++ GameLeaderboard/GameLeaderboard/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CSharpCourse")]
+[assembly: AssemblyTitle("GameLeaderboard")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CSharpCourse")]
+[assembly: AssemblyProduct("GameLeaderboard")]
 [assembly: AssemblyCopyright("Copyright ©  2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6eada295-6634-40fa-9919-e45b47115c9a")]
+[assembly: Guid("b993b9d0-5b6b-49f8-bc09-3603d23ae240")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
